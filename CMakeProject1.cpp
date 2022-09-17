5e69b6cdbcb94c5f4f37b05756adc772967a20ba
Notcher3 <62072039+nclol12@users.noreply.github.com>
2022-09-17 19:25:10 +0300

    Update CMakeProject1.cpp

--- CMakeProject1.cpp
+++ CMakeProject1.cpp
@@ -10,25 +10,24 @@
 #include<sstream>
 using namespace std;
 
-int main()//если запущен как обычно
+int main()
 {
 	string gmdsong;
 	string gmddata;
 	string gmdname;
-	//string gmdext = ".gmd";
-	//string gmdimport = "_imported";
-	//string gmdogorod; ойче
-	//Я сегодня пожарил бульбочку
 	stringstream gmds;
+        string gmdexl;
 	SetConsoleTitle("Level string into gdshare covnerter");
-	printf("\n thx misanthropez for idea)");//миза крутой 
-	//printf("\n i can`t stop thinking about balls"); нотчер3#8385 поехал крышей и ебёт рычаги
+	printf("\n thx misanthrope for idea)");
 
 	const int count = 1;
 	ofstream gmdw;
 
 	cout << "Please enter the level name:";
 	cin >> gmdname;
+        
+        
+        std::stringstream gmdexl = fmt::sprintf(".gmd");
 
 
     gmds <<gmdname;//<<gmdimport;
@@ -42,25 +41,11 @@ int main()//если запущен как обычно
 	cout << "Please enter the level string:";
 	cin >> gmddata;
 {
- ofstream MyFile(gmdname);	
- MyFile << "<d><k>kCEK</k><i>4</i><k>k2</k><s>" << (gmddata) << "</s><k>k3</k><s>WTI5dGJXVnVkSE1nWm1WaGRIVnlaU0J1YjNRZ2VXVjBJR2x1WTJ4MVpHVms=</s><k>k4</k><s>" << (gmddata) << "</s><k>k45</k><i>" << (gmdsong) << "</i><k>k13</k><t/><k>k21</k><i>2</i><k>k50</k><i>35</i></d>";
+ ofstream MyFile(gmdname.gmdexl);	
+ MyFile << "<d><k>kCEK</k><i>4</i><k>k2</k><s>" << (gmdname) << "</s><k>k3</k><s>WTI5dGJXVnVkSE1nWm1WaGRIVnlaU0J1YjNRZ2VXVjBJR2x1WTJ4MVpHVms=</s><k>k4</k><s>" << (gmddata) << "</s><k>k45</k><i>" << (gmdsong) << "</i><k>k13</k><t/><k>k21</k><i>2</i><k>k50</k><i>35</i></d>";
 }
 	; return 0;
 }
-
-
-
-	//cout << gmdname; Дiвять 
-	//std::ofstream outfile(gmdname.gmdext) Дiсять
-		//fstream gmdstuff ДIСЯТЬ!!!!!!!!11
-		//gmdstuff.open(gmdname.,)Дiвять?
-
-	//Белая армия, чёрный барон
-	//Снова готовят нам царский трон
-	//Но от тайги до британских морей
-	//Красная Армия всех сильней!
-
-		
 
 
 
